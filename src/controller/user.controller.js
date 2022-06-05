e1fc38699985879e4d35b23d958c952b9699ff73
Rodrigo Herrainz <rherrainz@gmail.com>
2022-06-05 20:12:55 -0300

    added put and delete methods

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -25,11 +25,35 @@ export const createUserController = (data) => {
         id: users[users.length -1].id + 1,
         name: data.name
     }
-
     users.push(userCreated)
     const newListUsers = [...users]
-
     fs.writeFileSync(userFilePath, JSON.stringify(newListUsers))
-
     return userCreated 
+}
+
+export const deleteUserController = (id) => {
+
+    let users = JSON.parse(fs.readFileSync(userFilePath, 'utf8'))
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === id) {
+          users.splice(i, 1);
+          break;
+        }
+    }
+    fs.writeFileSync(userFilePath, JSON.stringify(users))    
+return ("El usuario ",id," ha sido eliminado") 
+
+}
+export const updateUserController = (id,data) => {
+    console.log("DATA ==> ", data)
+    let users = JSON.parse(fs.readFileSync(userFilePath, 'utf8'))
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === id) {
+          users[i].name=(data.name);
+          break;
+        }
+    }
+    fs.writeFileSync(userFilePath, JSON.stringify(users))    
+return ("El usuario ",id," ha sido actualizado") 
+
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { findUsersController, getUserController, createUserController } from '../controller/user.controller.js'
+import { findUsersController, getUserController, createUserController, deleteUserController, updateUserController } from '../controller/user.controller.js'
 
 export const userRouter = express.Router()
 
@@ -21,5 +21,21 @@ userRouter.post('/', (req, res) => {
     const body = req.body
     console.log(body)
     const user = createUserController(body)
+    res.send(user)
+})
+
+userRouter.delete('/:id', (req, res) => {
+    const userId = Number(req.params.id)
+    const user = deleteUserController(userId)
+    if (!user) res.send('<h1>No Existe Usuario</h1>')
+    res.send(user)
+})
+
+userRouter.put('/:id', (req, res) => {
+    const body=req.body
+    console.log(body)
+    const userId = Number(req.params.id)
+    const user = updateUserController(userId,body)
+    if (!user) res.send('<h1>No Existe Usuario</h1>')
     res.send(user)
 })
